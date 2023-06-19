61c5a85cd3a8d8296c02460ebe5226cd28c62d3e
aalali <ahmedalali5294@gmail.com>
2023-06-19 05:29:14 +0300

    clean up branch

--- rowPrinter.go
+++ rowPrinter.go
@@ -3,49 +3,49 @@ package asciiart
 import "fmt"
 
 func RowPrinter(splitInput []string, alignment string, sourceFile []byte, f func([]byte, int) []byte) {
-	if alignment != "--align=justify"{
-	fullRowData := ""
-	rowlength := Findsize() - 2
-	rowlength2 := 1
-	for _, singleLine := range splitInput { // to print one line at a time
-		if singleLine != "" {
-			for i := 2; i < 10; i++ { // to print 8 lines of each character
-				for _, charRune := range singleLine { // to combine the prespective line of each char to the next
-					rowLocation := (((int(charRune) - 32) * 9) + i)
-					charRowData := f(sourceFile, rowLocation)
-					fullRowData = fullRowData + string(charRowData)
-				}
-				if alignment == "--align=left" { // to print if alignment =left
-					rowlength2 = rowlength - len(fullRowData)
-					fmt.Println(fullRowData)
-					fullRowData = ""
-				}
-				if alignment == "--align=right" { // to print if alignment =right
-					rowlength2 = rowlength - len(fullRowData)
-					for j := 0; j < rowlength2; j++ {
-						fmt.Print(" ")
+	if alignment != "--align=justify" {
+		fullRowData := ""
+		rowlength := Findsize() - 2
+		rowlength2 := 1
+		for _, singleLine := range splitInput { // to print one line at a time
+			if singleLine != "" {
+				for i := 2; i < 10; i++ { // to print 8 lines of each character
+					for _, charRune := range singleLine { // to combine the prespective line of each char to the next
+						rowLocation := (((int(charRune) - 32) * 9) + i)
+						charRowData := f(sourceFile, rowLocation)
+						fullRowData = fullRowData + string(charRowData)
 					}
-					fmt.Println(fullRowData)
-					fullRowData = ""
-				}
-				if alignment == "--align=center" { // to print if alignment = center
-					rowlength2 = (rowlength - len(fullRowData)) / 2
-					for j := 0; j < rowlength2; j++ {
-						fmt.Print(" ")
+					if alignment == "--align=left" { // to print if alignment =left
+						rowlength2 = rowlength - len(fullRowData)
+						fmt.Println(fullRowData)
+						fullRowData = ""
 					}
-					fmt.Print(fullRowData)
-					fullRowData = ""
-					for j := 0; j < rowlength2; j++ {
-						fmt.Print(" ")
+					if alignment == "--align=right" { // to print if alignment =right
+						rowlength2 = rowlength - len(fullRowData)
+						for j := 0; j < rowlength2; j++ {
+							fmt.Print(" ")
+						}
+						fmt.Println(fullRowData)
+						fullRowData = ""
 					}
-					fmt.Print("\n")
+					if alignment == "--align=center" { // to print if alignment = center
+						rowlength2 = (rowlength - len(fullRowData)) / 2
+						for j := 0; j < rowlength2; j++ {
+							fmt.Print(" ")
+						}
+						fmt.Print(fullRowData)
+						fullRowData = ""
+						for j := 0; j < rowlength2; j++ {
+							fmt.Print(" ")
+						}
+						fmt.Print("\n")
+					}
 				}
+			} else {
+				fmt.Print("\n")
 			}
-		} else {
-			fmt.Print("\n")
 		}
-	} 
 	} else {
 		RowPrinterjustify(splitInput, alignment, sourceFile, RowParser)
-	}	
+	}
 }
--- rowPrinterjustify.go
+++ rowPrinterjustify.go
@@ -3,19 +3,18 @@ package asciiart
 import "fmt"
 
 func RowPrinterjustify(splitInput []string, alignment string, sourceFile []byte, f func([]byte, int) []byte) {
-	fullRowData := ""
-	rowlength := Findsize() - 2
-	extraspaces := ""
-
 	for _, singleLine := range splitInput { // to print one line at a time
+		fullRowData := ""
+		rowlength := Findsize() - 2
+		extraspaces := ""
 		lengthofwords := 0
 		numberofwords := 0
-		if singleLine != "" {			
+		if singleLine != "" {
 			for i := 2; i < 10; i++ { // to print 8 lines of each character
 				for _, charRune := range singleLine { // to combine the prespective line of each char to the next
 					rowLocation := (((int(charRune) - 32) * 9) + i)
 					charRowData := f(sourceFile, rowLocation)
-				if (charRune == 32) && i == 2 {
+					if (charRune == 32) && i == 2 {
 						numberofwords++
 					}
 					if i == 2 && charRune != 32 {
@@ -23,35 +22,33 @@ func RowPrinterjustify(splitInput []string, alignment string, sourceFile []byte,
 					}
 				}
 			}
-	
-		if numberofwords == 0  {
-			numberofwords++
-		}
-				lengthbetweenwords := ((rowlength - lengthofwords) / numberofwords)
-				extraspaces = ""
-		for i := 0; i < lengthbetweenwords; i++ {
-			extraspaces = extraspaces + " "
-		}
-				for i := 2; i < 10; i++ { // to print 8 lines of each character
-					for _, charRune := range singleLine { // to combine the prespective line of each char to the next
-						rowLocation := (((int(charRune) - 32) * 9) + i)
-						charRowData := f(sourceFile, rowLocation)
-						
-						if alignment == "--align=justify" && (charRune == ' ') {
-							fullRowData = fullRowData + extraspaces
-						}
-						if charRune != 32 {
-							fullRowData = fullRowData + string(charRowData)
-						}
-				}
-					fmt.Println(fullRowData)
-				
 
-					fullRowData = ""
-				}
-			} else {
-				fmt.Print("\n")
+			if numberofwords == 0 {
+				numberofwords++
 			}
+			lengthbetweenwords := ((rowlength - lengthofwords) / numberofwords)
+			extraspaces = ""
+			for i := 0; i < lengthbetweenwords; i++ {
+				extraspaces = extraspaces + " "
 			}
-			 }
+			for i := 2; i < 10; i++ { // to print 8 lines of each character
+				for _, charRune := range singleLine { // to combine the prespective line of each char to the next
+					rowLocation := (((int(charRune) - 32) * 9) + i)
+					charRowData := f(sourceFile, rowLocation)
 
+					if charRune == ' ' {
+						fullRowData = fullRowData + extraspaces
+					}
+					if charRune != 32 {
+						fullRowData = fullRowData + string(charRowData)
+					}
+				}
+				fmt.Println(fullRowData)
+
+				fullRowData = ""
+			}
+		} else {
+			fmt.Print("\n")
+		}
+	}
+}
